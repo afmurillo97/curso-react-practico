550f944f3a4ccab864be93a5e7066ca6c5089ad7
afmurillo97 <afmurillo97@gmail.com>
2024-06-07 18:13:49 -0500

    Fixed a bug caused by the API change

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -42,7 +42,7 @@ const Card = ({ data }) => {
   return (
     <div
       className='bg-white cursor-pointer w-56 h-60 rounded-lg'
-      onClick={() => showProduct(data.data)}>
+      onClick={() => showProduct(data)}>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.category}</span>
         <img className='w-full h-full object-cover rounded-lg' src={data.image} alt={data.title} />
